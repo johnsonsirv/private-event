f588a9ee4491b5f8e78538d89c5f01a5c9713b78
johnsonsirv <johnsonsirv@gmail.com>
2019-09-17 22:58:16 +0000

    all tests for login pass

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,14 @@ class SessionsController < ApplicationController
 	end
 	
 	def create
-		
+		user = User.find_by_username(params[:session][:username].downcase)
+		if user
+			sign_in user
+			redirect_to users_home_path
+		else
+			flash.now[:login_error] = "Username not Found"
+			render :new
+		end
 	end
 	
 	def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 	
 	get '/signup', to: 'users#new'
 	get '/users/home', to: 'users#show'
+	get '/login', to: 'sessions#new'
+	post '/login', to: 'sessions#create'
 	
 	resources :users, only: [:new, :create]
 end
